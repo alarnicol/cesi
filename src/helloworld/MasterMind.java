0f591993c9ac71c38c3ccd16f70ee49557c7dece
alarnicol <al.larnicol@gmail.com>
2016-07-05 13:55:29 +0200

    Add files via upload
    avec random

---
+++ src/helloworld/MasterMind.java
@@ -0,0 +1,36 @@
+package helloworld;
+
+import java.io.IOException;
+
+public class MasterMind {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Mm2 un_master ;
+		int[] resultat ;
+		boolean fini = false ;
+		char clavier ;
+		
+		try {
+			un_master = new Mm2() ;
+			un_master.OrdiJoue();
+			un_master.JoueurJoue();
+			resultat = un_master.ResultatJeu();
+
+            
+			while (!fini) {
+				System.out.println( "bien placé:" + resultat[0] + "; couleurs présentes:" + resultat[1] );
+	
+				System.out.println( "c: continue; espace : fin") ;
+				clavier = (char) System.in.read() ;
+				if (clavier == ' ' ) fini = true;
+			}
+			System.out.println( "Fin du jeu") ;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+
---
+++ src/helloworld/TestArrays.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+public class TestArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] islande=new String[4];
+		islande[0]= "bermude";
+		islande[1]= "fidi";
+		islande[2]= "aorea";
+		islande[3]= "cozumel";
+		int[] index=new int[4];
+		index[0]=1;
+		index[1]=2;
+		index[2]=3;
+		index[3]=4;
+		int y=0;
+
+	}
+
+}
